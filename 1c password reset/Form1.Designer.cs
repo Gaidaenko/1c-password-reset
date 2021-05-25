bb1f6c1ebdd89b9bb260455343f650ae0f73b48e
Yura <y.gaidaenko@agrifirm.com>
2021-05-25 21:53:38 +0300

    отключение пользователей 1с

--- 1c password reset/Form1.Designer.cs
+++ 1c password reset/Form1.Designer.cs
@@ -31,35 +31,88 @@ namespace _1c_password_reset
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(319, 70);
+            this.button1.Location = new System.Drawing.Point(21, 54);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(218, 89);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "ШАГ 1. Отключить пользователей 1с.";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(343, 176);
+            this.label1.Location = new System.Drawing.Point(536, 104);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(129, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Имя SQL базы";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(666, 104);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(204, 39);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(666, 184);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(204, 41);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(536, 189);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Имя сервера";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 365);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Статус";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1142, 750);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -71,6 +124,10 @@ namespace _1c_password_reset
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- 1c password reset/Form1.cs
+++ 1c password reset/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,24 +21,75 @@ namespace _1c_password_reset
             InitializeComponent();
         }
 
-        public static string file1c = @"C:\Program Files\1cv8\common\1cestart.exe";
+        public static string cestart = @"C:\Program Files\1cv8\common\1cestart.exe";
+        public void SqlConnection()
+        {
+            string dbName = richTextBox1.Text;
+            string serverName = richTextBox2.Text;          
+            string connectionString = @"Data Source = " + serverName + ";Initial Catalog="+ dbName +";Integrated Security=True";
+
+            SqlConnection connect = new SqlConnection(connectionString);
+          
+            string sqlUsersRename = @"EXEC sp_rename 'v8users','v8users_tmp'";
+            string sqlParamsRename = @"UPDATE Params SET FileName = 'users.usr_tmp' WHERE FileName = 'users.usr'";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connect.Open();
+                    SqlCommand usersRename = new SqlCommand(sqlUsersRename, connect);
+                    int number = usersRename.ExecuteNonQuery();
+                                        
+                    SqlCommand paramsRename = new SqlCommand(sqlParamsRename, connect);
+                    int number2 = paramsRename.ExecuteNonQuery();
+
+                    label3.Text = "Пользователи 1c отключены!";
+                }
+                catch (SqlException e)
+                {
+                    label3.Text = "Подключение у SQL серверу не удалось! Проверьте правильность написания имени сервера, или базы SQL!";
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (File.Exists(file1c))
-            {
-               
-                Process.Start(file1c);
+            SqlConnection();
 
-            }
-            else if (!File.Exists(file1c))
-            {
-                label1.Text = "Файл для запуска 1с не существует!";
-            }
+            Thread.Sleep(2000);
 
+              if (File.Exists(cestart))
+              {               
+                  Process.Start(cestart);
+              }
+              else if (!File.Exists(cestart))
+              {
+                  label1.Text = "Файл для запуска 1с не существует. Попробуйсте запустить 1с вручную!";
+              } 
         }
 
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
